818b2b21ca43a4866907652d9dfc168cc94b6f9f
Abhinay <abhisubbudhi@gmail.com>
2026-09-08 21:30:26 +0530

    Intial commit

---
+++ src/main/java/com/portfolio/app/Main.java
@@ -0,0 +1,13 @@
+package com.portfolio.app;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("=============================");
+        System.out.println(" Welcome to Portfolio Project");
+        System.out.println(" ============================");
+    }
+} 
