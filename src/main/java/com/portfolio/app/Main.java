f1fafd4733360f7a3de696efab06a1a35d8ed36a
Abhinay <abhisubbudhi@gmail.com>
2026-09-12 10:51:38 +0530

    Intial commit

--- src/main/java/com/portfolio/app/Main.java
+++ src/main/java/com/portfolio/app/Main.java
@@ -1,17 +1,13 @@
 package com.portfolio.app;
-import com.portfolioproject.model.*;
-
- 
- 
 public class Main {
+	
     public static void main(String[] args)
     {
         System.out.println("=============================");
-        System.out.println("  Portfolio Management Project");
+        System.out.println(" Portfolio Management Project");
         System.out.println(" ============================");                                                    
-        User user=new User("101","Abhi","Abhi@gmail.com");
-        System.out.println("User ID: =" + user.getUserid());
-        System.out.println("Name:" + user.getName());  
-        System.out.println("Email: " + user.getEmail());
+        
+        
+       
     }
 }  
--- src/main/java/com/portfolio/app/app/Main.java
+++ src/main/java/com/portfolio/app/app/Main.java
@@ -1,10 +1,16 @@
 package com.portfolio.app.app;
-
+import com.portfolio.app.model.*;
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("============================");
+		System.out.println("Portfolio Management Project");
+		System.out.println("=============================");
+        User user=new User("101","Abhi","Abhi@gmail.com");
+        System.out.println("User ID: " + user.getUserid());
+        System.out.println("Name: " + user.getName());
+        System.out.println("Email: " + user.getEmail());
 	}
 
 }
